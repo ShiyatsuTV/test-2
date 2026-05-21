8df0987c5a97172afe9a9dc708979d2167c8d040
ShiyatsuTV <shiyatsu70@gmail.com>
2026-05-21 15:45:44 +0200

    Init

---
+++ src/main/java/com/test/Main.java
@@ -0,0 +1,8 @@
+package com.test;
+
+// adapte l'import à ta vraie classe dans la lib logger
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Test de consommation depuis Nexus");
+    }
+}
